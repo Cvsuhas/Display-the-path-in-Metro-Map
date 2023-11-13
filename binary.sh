180c714369919fe167584c29be1b850d5d501843
Cvsuhas <suhascv2002@gmail.com>
2023-11-13 20:58:08 +0530

    added endline

--- binary.sh
+++ binary.sh
@@ -13,4 +13,4 @@ make
 
 echo "**Build Successfull"
 
-echo "to run the executable go to binary directory and execute ./display_map to enter the Todo_Lists"
+echo "to run the executable go to binary directory and execute ./display_map
